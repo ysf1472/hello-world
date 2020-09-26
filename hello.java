13c0127bb2f3013a1898a248d8c60930a513647e
ysf1472 <64108009+ysf1472@users.noreply.github.com>
2020-09-25 20:32:07 -0400

    first code
    hello world!!

---
+++ hello.java
@@ -0,0 +1,12 @@
+public class hello
+{
+    public static void   main(String[] args){
+        System.out.println("Hello, Yusuf ");
+        int num1= (5 +9) *3 ;
+        int num2= 12;
+        int num3=13/2;
+        int total=  num1+num2-num3;
+        System.out.println("my first number " +total);
+        int
+    }
+}
